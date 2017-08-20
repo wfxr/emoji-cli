868cf112d13dd68e27ef5cb154bb77cf79e65cc5
Wenxuan <wenxuangm@gmail.com>
2017-08-20 18:29:59 +0800

    Support custom keybind

--- emoji-cli.zsh
+++ emoji-cli.zsh
@@ -1,10 +1,10 @@
 #!/usr/bin/env zsh
 
-function __emoji() {
+function emojify::cli() {
     local emoji=`$0:A:h/emojify.rb -l | column -t | fzf | awk '{print ":"$1":"}'`
     BUFFER=$BUFFER$emoji
     CURSOR=$#BUFFER
     zle redisplay
 }
-zle     -N    __emoji
-bindkey '^[m' __emoji
+zle -N emojify::cli
+bindkey ${EMOJI_CLI_KEYBIND:='^[m'} emojify::cli
